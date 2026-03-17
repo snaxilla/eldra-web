7102edc1660bf347e7a7d1208d04ad0163715c20
Snaxilla <sam@theledouxs.com>
2026-03-17 20:20:24 +0000

    Add Directus token runtime config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   modules: ['@nuxt/ui'],
   css: ['~/assets/css/main.css'],
   runtimeConfig: {
+    directusToken: process.env.DIRECTUS_TOKEN || '',
     public: {
       directusUrl: process.env.NUXT_PUBLIC_DIRECTUS_URL || 'http://ledouxvps-directus-269351-187-77-194-11.traefik.me'
     }
